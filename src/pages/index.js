bb6f2ff69b6a14bf79d5c1272e629b701fa1971a
yoichi-aoyama <aoyama.yoichi@gmail.com>
2022-05-26 01:36:39 +0900

    update: テーマ変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button, Jumbotron } from "react-bootstrap";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { Link } from "gatsby";
@@ -7,16 +6,13 @@ import { Link } from "gatsby";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Jumbotron>
       <h1>Hello, world!</h1>
       <p>
         This is an example of the <tt>gatsby-starter-basic-bootstrap</tt>{' '}
         starter. 
       </p>
       <p>
-        <Button as={Link} to="/about" variant="primary">Learn more</Button>
       </p>
-    </Jumbotron>
   </Layout>
 );
 
